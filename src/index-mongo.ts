9dde162b52febfacdcca462d5930a0a1bfe0d8c5
menelinda <maria.menegassi@estudante.ifms.edu.br>
2025-09-10 12:48:41 -0300

    Correção de código utilizando mongo

---
+++ src/index-mongo.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import 'dotenv/config'
+import {MongoClient} from 'mongodb'
+
+async function startServer() {
+const client = new MongoClient(process.env.MONGO_URI!)
+await client.connect()
+const db = client.db(process.env.MONGO_DB)
+
+
+
+const app = express()
+//explique o que esse middleware faz com que o express faça o perse do body da requisição para json 
+app.use(express.json())
+
+// criando uma rota para acesso pelo navegador
+app.get('/produtos', async (req, res) => {
+   const produtos = await db.collection('produtos').find().toArray()
+    res.json(produtos)
+})
+
+// Criando o servidor na porta 8000
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
+}
+
+startServer()
